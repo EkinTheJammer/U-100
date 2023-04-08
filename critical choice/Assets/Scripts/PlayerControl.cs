96058845866851d2bb10fc9f911cecb810196e02
esrasahinn <73536858+esrasahinn@users.noreply.github.com>
2023-04-08 18:24:46 +0300

    player controller

---
+++ critical choice/Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    
+    Vector3 firstPos, endPos;
+    public float playerSpeed;
+
+
+        void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            firstPos = Input.mousePosition;
+        }
+        else if (Input.GetMouseButton(0))
+        {
+            endPos = Input.mousePosition;
+
+            float farkX = endPos.x - firstPos.x;
+
+            transform.Translate(farkX * Time.deltaTime * playerSpeed /100, 0, 0);
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            firstPos = Vector3.zero;
+            endPos = Vector3.zero;
+        }
+    }
+}
